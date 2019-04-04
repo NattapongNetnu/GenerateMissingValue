c2e12d1b077cbce8208e641d3f76bae56807e6d8
Nattapong Netnu <nattapong.netnu@gmail.com>
2019-04-05 01:20:06 +0700

    Finish For Group Project 1

---
+++ Fillna.py
@@ -0,0 +1,16 @@
+# data['month'].iloc[random.sample(range(500), 100)] = np.nan
+# data['date_of_month'].iloc[random.sample(range(500), 100)] = np.nan
+# data['day_of_week'].iloc[random.sample(range(500), 100)] = np.nan
+# data['births'].iloc[random.sample(range(500), 100)] = np.nan
+
+# data.to_csv("./Exercise7_edit.csv", index=False)
+
+# data['year'] = data['year'].fillna(1994)
+# data['month'] = data['month'].fillna(1)
+# data['date_of_month'] = data['date_of_month'].fillna(4)
+# data['day_of_week'] = data['day_of_week'].fillna(7)
+# data['births'] = data['births'].fillna(123)
+# print(data.head(10))
+# data['date_of_month'].iloc[random.sample(range(500), 100)] = np.nan
+# data['day_of_week'].iloc[random.sample(range(500), 100)] = np.nan
+# data['births'].iloc[random.sample(range(500), 100)] = np.na
