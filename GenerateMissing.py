ae6abe2db29685e7d37b1e9658e5e3269d427a5f
Nattapong Netnu <nattapong.netnu@gmail.com>
2019-04-05 02:00:32 +0700

    Group Project 1 Finish v 1.2

--- GenerateMissing.py
+++ GenerateMissing.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 import random
-data = pd.read_csv("./datasets/e-commerce/data.csv", encoding="utf-8")
+data = pd.read_csv("./datasets/e-commerce/data.csv", encoding="utf-8", index_col=[0])
 
+random.seed(6)
 
-columns = random.sample(population=list(data.columns), k=3)
+columns = random.sample(population=list(data.columns), k=4)
 count = data.shape[0]
 countMissing = int(count / 10)
 
@@ -15,4 +16,4 @@ print(columns)
 print(data)
 
 
-# data.to_csv("./datasets/e-commerce/data.csv", index=False, encoding='utf-8')
+data.to_csv("./datasets/e-commerce/data_missing.csv", index=False, encoding='utf-8')
